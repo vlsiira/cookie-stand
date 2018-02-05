195874b54a2b663539b15e9368b423bed1aaa476
Valerie Siira <vlsiira@vt.edu>
2018-02-05 14:59:50 -0800

    added object literals for each shop

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const airportLoc = {
+    minHrlyCust: 23,
+    maxHrlyCust: 65,
+    avgCookiesCust: 6.3
+};
+
+const pioneerSqLoc = {
+    minHrlyCust: 3,
+    maxHrlyCust: 24,
+    avgCookiesCust: 1.2
+};
+
+const powellsLoc = {
+    minHrlyCust: 11,
+    maxHrlyCust: 38,
+    avgCookiesCust: 3.7
+};
+
+const stJohnsLoc = {
+    minHrlyCust: 20,
+    maxHrlyCust: 38,
+    avgCookiesCust: 2.3
+};
+
+const waterfrontLoc = {
+    minHrlyCust: 2,
+    maxHrlyCust: 16,
+    avgCookiesCust: 4.6
+};
+

4db578ffa5b0cd5cc3990ef375a0162a5205fcf3
Valerie Siira <vlsiira@vt.edu>
2018-02-05 16:04:37 -0800

    added method to generate random number of customers per hr

--- app.js
+++ app.js
@@ -3,30 +3,60 @@
 const airportLoc = {
     minHrlyCust: 23,
     maxHrlyCust: 65,
-    avgCookiesCust: 6.3
+    avgCookiesCust: 6.3,
+    getRandomNum: function () {
+        this.minHrlyCust = Math.ceil(this.minHrlyCust);
+        this.maxHrlyCust = Math.floor(this.maxHrlyCust);
+        return Math.floor(Math.random() * (this.maxHrlyCust - this.minHrlyCust + 1)) + this.minHrlyCust;
+    }
 };
+console.log(airportLoc.getRandomNum());
 
 const pioneerSqLoc = {
     minHrlyCust: 3,
     maxHrlyCust: 24,
-    avgCookiesCust: 1.2
+    avgCookiesCust: 1.2,
+    getRandomNum: function () {
+        this.minHrlyCust = Math.ceil(this.minHrlyCust);
+        this.maxHrlyCust = Math.floor(this.maxHrlyCust);
+        return Math.floor(Math.random() * (this.maxHrlyCust - this.minHrlyCust + 1)) + this.minHrlyCust;
+    }
 };
+console.log(pioneerSqLoc.getRandomNum());
 
 const powellsLoc = {
     minHrlyCust: 11,
     maxHrlyCust: 38,
-    avgCookiesCust: 3.7
+    avgCookiesCust: 3.7,
+    getRandomNum: function () {
+        this.minHrlyCust = Math.ceil(this.minHrlyCust);
+        this.maxHrlyCust = Math.floor(this.maxHrlyCust);
+        return Math.floor(Math.random() * (this.maxHrlyCust - this.minHrlyCust + 1)) + this.minHrlyCust;
+    }
 };
+console.log(powellsLoc.getRandomNum());
 
 const stJohnsLoc = {
     minHrlyCust: 20,
     maxHrlyCust: 38,
-    avgCookiesCust: 2.3
+    avgCookiesCust: 2.3,
+    getRandomNum: function () {
+        this.minHrlyCust = Math.ceil(this.minHrlyCust);
+        this.maxHrlyCust = Math.floor(this.maxHrlyCust);
+        return Math.floor(Math.random() * (this.maxHrlyCust - this.minHrlyCust + 1)) + this.minHrlyCust;
+    }
 };
+console.log(stJohnsLoc.getRandomNum());
 
 const waterfrontLoc = {
     minHrlyCust: 2,
     maxHrlyCust: 16,
-    avgCookiesCust: 4.6
+    avgCookiesCust: 4.6,
+    getRandomNum: function () {
+        this.minHrlyCust = Math.ceil(this.minHrlyCust);
+        this.maxHrlyCust = Math.floor(this.maxHrlyCust);
+        return Math.floor(Math.random() * (this.maxHrlyCust - this.minHrlyCust + 1)) + this.minHrlyCust;
+    }
 };
+console.log(waterfrontLoc.getRandomNum());
 

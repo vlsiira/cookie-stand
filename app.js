87a4ab781a869c83b0f484d0f6070528dcc39a3a
Valerie Siira <vlsiira@vt.edu>
2018-02-07 17:36:02 -0800

    first 2 row of table working

--- app.js
+++ app.js
@@ -39,16 +39,31 @@ function CookieStand (minHrlyCust, maxHrlyCust, avgCookiesCust) {
 // working the way it's supposed to.
 
 // ^^^TODO push random customer number * avg cookie number into amount cookies array
-
 CookieStand.prototype.calc = function () {
     let min = Math.ceil(this.minHrlyCust);
     let max = Math.floor(this.maxHrlyCust);
     let hrlySale;
+
+    this.amntCookiesPurch.push('Airport');
+    
     //for (let i = 0; i < this.hourOfDay.length; i++) {
-    for (let i = 0; i < 15; i++) {
-        hrlySale = Math.floor((Math.floor(Math.random() * (max - min)) + min) * this.avgCookiesCust);
-        this.amntCookiesPurch.push(hrlySale); 
-    }
+        for (let i = 0; i < 15; i++) {
+            hrlySale = Math.floor((Math.floor(Math.random() * (max - min)) + min) * this.avgCookiesCust);
+            this.amntCookiesPurch.push(hrlySale); 
+        }
+        const hour = ['', '6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
+        for (let i = 0; i < hour.length; i++) {
+            const tableBody = document.querySelector('tbody');
+            const newTd = document.createElement('td');
+            newTd.textContent = this.amntCookiesPurch[i];
+            tableBody.appendChild(newTd);
+         }
+        for (let i = 0; i < hour.length; i++) {
+            const tableTopRow = document.getElementById('tableTopRow');
+            const newTh = document.createElement('th');
+            newTh.textContent = hour[i];
+            tableTopRow.appendChild(newTh);
+        }
 }
 
 CookieStand.prototype.list = function () {
@@ -69,9 +84,30 @@ airportLoc.list();
 ///////////////////////////////////////////////////////////////////////////////////////
 
 
+//const tableIds = ['tableHeading', 'tableAirport', 'tablePiSq', 'tablePowells', 'tableStJohns', 'tableWaterfront'];
+const tableLoc = ['Airport', 'Pioneer Square', 'Powell\'s', 'St. John\'s', 'Waterfront'];
 
+
+// for (let i = 0; i < hour.length; i++) {
+//     const tableBody = document.querySelector('tbody');
+//     const newTd = document.createElement('td');
+//     newTd.textContent = this.amntCookiesPurch[i];
+//     tableBody.appendChild(newTd);
+// }
+
+
+
+// for (let i = 0; i < hour.length; i++) {
+//     const tableRows = getElementsByTagName('tr');
+//     const newTh = document.createElement('th');
+//     newTh.textContent = tableLoc[i];
+//     tableRows.appendChild(newTh);
+// }
 
 // TODO create table
+//       -times in array              -this.hourOfDay
+//       -loop through array
+//       -use getElement
 //       -make data display in table
 
 // TODO create new instances of CookieStand for other stores

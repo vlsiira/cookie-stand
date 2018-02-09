eeb8270134b35f25312b5a0711f102edf63d1307
Valerie Siira <vlsiira@vt.edu>
2018-02-08 17:23:54 -0800

    finished table, created instances of all stores, added total column

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// TODO - Move Pioneer Square line under Airport line.  I think I need to do something with
-//        nextSibling and an array, in the second for loop of the calc function.
-//      - Create instances of other locations, make sure they look right.
-//      - Calculate totals and add to table.
-
 ///////////////Constructor/////////////////////////////////////////////////
 function CookieStand (minHrlyCust, maxHrlyCust, avgCookiesCust, storeName) {
     this.minHrlyCust = minHrlyCust;
@@ -20,38 +15,96 @@ CookieStand.prototype.calc = function () {
     let hrlySale;
 
     this.amntCookiesPurch.push(this.storeName);
+
     for (let i = 0; i < 15; i++) {
         hrlySale = Math.floor((Math.floor(Math.random() * (max - min)) + min) * this.avgCookiesCust);
         this.amntCookiesPurch.push(hrlySale);
     }
-    for (let i = 0; i < 16; i++) {
-        const tableBody = document.querySelector('tbody');
-        const newTd = document.createElement('td');
-        newTd.textContent = this.amntCookiesPurch[i];
-        tableBody.appendChild(newTd);
-    }
 }
 
 CookieStand.prototype.makeTableHeading = function () {
-        const hour = ['Store', '6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
-        for (let i = 0; i < hour.length; i++) {
-            const tableTopRow = document.getElementById('tableTopRow');
-            const newTh = document.createElement('th');
-            newTh.textContent = hour[i];
-            tableTopRow.appendChild(newTh);
+    const hour = ['Store', '6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total:'];
+    for (let i = 0; i < hour.length; i++) {
+        const tableTopRow = document.getElementById('tableTopRow');
+        const newTh = document.createElement('th');
+        newTh.textContent = hour[i];
+        tableTopRow.appendChild(newTh);
+    }
+}
+
+CookieStand.prototype.insertRows = function () {
+    let count = 0;
+    for (let i = -1; i < 17; i++) {
+        const newRow = document.createElement('tr');
+        const newTh = document.getElementById('tableTopRow');
+        const newTd = document.createElement('td');
+        if (i < 0) {
+            newTh.appendChild(newRow);              
+        } else if (i === 0) {
+            newTd.textContent = this.amntCookiesPurch[i];
+            newTh.appendChild(newTd);
+        } else if (i === 16) {
+            newTd.textContent = count;
+            newTh.appendChild(newTd);
+            console.log(count);
+        } else {
+            count = count + this.amntCookiesPurch[i];
+            newTd.textContent = this.amntCookiesPurch[i];
+            newTh.appendChild(newTd);
         }
+    }
 }
 
 const airportLoc = new CookieStand(23, 65, 6.3, 'Airport');
-airportLoc.calc();
 airportLoc.makeTableHeading();
+airportLoc.calc();
+airportLoc.insertRows();
 
 const pioneerSqLoc = new CookieStand(3, 24, 1.2, "Pioneer Square");
 pioneerSqLoc.calc();
+pioneerSqLoc.insertRows();
 
+const powellsLoc = new CookieStand(11, 38, 3.7, 'Powell\'s');
+powellsLoc.calc();
+powellsLoc.insertRows();
+
+const stJohnsLoc = new CookieStand(20, 38, 2.3, 'St. John\'s');
+stJohnsLoc.calc();
+stJohnsLoc.insertRows();
+
+const waterfrontLoc = new CookieStand(2, 16, 4.6, 'Waterfront');
+waterfrontLoc.calc();
+waterfrontLoc.insertRows();
 
 ///////////////////////////////////////////////////////////////////////////////////////
-
+// const newTr = document.createElement('tr');
+//     tableTopRow.appendChild(newTr);
+//     const rowsToTable = document.querySelectorAll('tr');
+//     for (let i = 0; i < rowsToTable.length; i++) {
+    //         const newTd = document.createElement('td');
+    //         newTd.textContent = this.amntCookiesPurch[i];
+    //         rowsToTable[i].appendChild(newTd);
+    //     }
+    // }
+    
+//const tableBody = document.querySelector('tbody');
+        // const rowsToTable = document.querySelectorAll('tr');
+        // for (let i = 0; i < rowsToTable.length; i++) {
+            //     rowsToTable[i].appendChild(newTd);
+            // }
+        //makes 5 tr
+        // for (let i = 0; i < 5; i++) {
+        //     const newTr = document.createElement('tr');
+        //     tableTopRow.appendChild(newTr);
+        // }
+    
+        // // make 15 td under each tr.
+        // for (let i = 0; i < 15; i++) {
+        //     const newTd = document.createElement('td');
+        //     newTr.appendChild(newTd);
+        //     newTd.textContent = this.amntCookiesPurch[i];
+        // }
+        
 //const tableIds = ['tableHeading', 'tableAirport', 'tablePiSq', 'tablePowells', 'tableStJohns', 'tableWaterfront'];
 //pioneerSqLoc.list();
 //airportLoc.list();
@@ -247,3 +300,7 @@ pioneerSqLoc.calc();
 // console.log(airportLoc.amntCookiesPurch);
 //console.log(airportLoc.hourOfDay);
 // 
+// TODO - Move Pioneer Square line under Airport line.  I think I need to do something with
+//        nextSibling and an array, in the second for loop of the calc function.
+//      - Create instances of other locations, make sure they look right.
+//      - Calculate totals and add to table.
